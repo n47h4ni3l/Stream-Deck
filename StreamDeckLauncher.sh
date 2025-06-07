69c8789b7127f5810068d0648b6afc094d9d7058
n47h4ni3l <74298967+n47h4ni3l@users.noreply.github.com>
2025-06-07 15:57:29 +0930

    Update StreamDeckLauncher.sh

--- StreamDeckLauncher.sh
+++ StreamDeckLauncher.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Stream Deck Launcher Script
-# Launches Electron app in fullscreen
+# Stream Deck Launcher Script - Final Version
+# Launches Electron app with bundled Chromium launcher support
 
-# Optional: Set working directory
+# Set working directory to script location
 cd "$(dirname "$0")"
 
-# Run Electron app
+# Make sure Chromium AppImage is executable
+chmod +x chromium/Chromium-x86-64.AppImage
+
+# Run Electron app (fullscreen / Deck-friendly)
 npx electron . --enable-features=OverlayScrollbar --kiosk
